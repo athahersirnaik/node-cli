55a71e3a2d584b6338288ad8954ff3a20a3cebb9
Athaher Sirnaik <athaher@outlook.com>
2018-08-22 22:43:30 +0530

    Added ncp

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 const inquirer = require("inquirer");
 const chalk = require("chalk");
 const shell = require("shelljs");
+var ncp = require('ncp').ncp;
 
 const askQuestions = () => {
     const questions = [
@@ -14,7 +15,7 @@ const askQuestions = () => {
       },
       {
         type: "input",
-        name: "FILENAME",
+        name: "NAME",
         message: "Please specify the name:"
       },
     ];
@@ -23,14 +24,36 @@ const askQuestions = () => {
 
 const inprogress = (option) => {
   console.log(
-    chalk.white.bgBlue(`Creating ${option} ...`)
+    chalk.black.bgBlue(`Creating ${option} ...`)
   );
 };
 
 const run = async () => {
     const answers = await askQuestions();
-    const { OPTION } = answers;
-    inprogress(OPTION)
+    const { OPTION, NAME } = answers;
+
+    if(!NAME) {
+      console.log(
+        chalk.white.bgRed("Please enter a name of "+ OPTION +". And try again")
+      )
+      return;
+    }
+    
+    if(OPTION == "Component") {
+      inprogress(OPTION)
+      ncp("templates/component","packages/dxp-"+NAME, function (err) {
+        if(err) {
+          return console.log(err)
+        }
+        console.log(
+          chalk.black.bgGreen(`Created ${NAME} ${OPTION}`)
+        )
+      } )
+    } else if(OPTION == "Interface") {
+      console.log(
+        chalk.black.bgYellow(`Not yet implemented`)
+      )
+    }
 }
 
 run();
